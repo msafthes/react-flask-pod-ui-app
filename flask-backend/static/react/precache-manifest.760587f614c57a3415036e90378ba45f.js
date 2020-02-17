fd0f180cbb5ef436e8b62ab3f60fa2a351cadaea
Martin Safar <xsafar1@fi.muni.cz>
2020-02-17 18:36:04 +0100

    basic react served by flask setup

---
+++ flask-backend/static/react/precache-manifest.760587f614c57a3415036e90378ba45f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb825e644d72f0bd722ab3805be09f6",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "baa81b1cb6a5f055ed96",
+    "url": "/static/react/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "944b9cf7615e1069e314",
+    "url": "/static/react/js/2.53a8c91c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/react/js/2.53a8c91c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa81b1cb6a5f055ed96",
+    "url": "/static/react/js/main.0dc9fc4c.chunk.js"
+  },
+  {
+    "revision": "8c5658d9b6066d4536fe",
+    "url": "/static/react/js/runtime-main.92776e4e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ react-app/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+declare global {
+  interface Window {
+      token:String;
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>My Token = {window.token}</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
