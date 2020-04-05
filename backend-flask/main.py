224a8d16f7a9a0e235a7e15bc5f7a3c437a79ba5
Martin Safar <xsafar1@fi.muni.cz>
2020-04-05 21:41:29 +0200

    Testing --format

--- backend-flask/main.py
+++ backend-flask/main.py
@@ -23,39 +23,39 @@ stores = [
 # GET /
 @app.route('/', methods=['GET', 'POST'])
 def get_stores():
-    process = subprocess.run(['echo', 'Trying echo command'],
-                             stdout=subprocess.PIPE,
-                             universal_newlines=True)
-    print("process:")
-    print(process.stdout)
-
-    process2 = subprocess.run(['ls'],
-                              stdout=subprocess.PIPE,
-                              universal_newlines=True)
-    print("process2:")
-    print(process2.stdout)
-
-    # process3 = subprocess.run(['ls -a'],
+    # process = subprocess.run(['echo', 'Trying echo command'],
     #                          stdout=subprocess.PIPE,
     #                          universal_newlines=True)
-    # print("process3:")
-    # print(process3.stdout)
+    # print("process:")
+    # print(process.stdout)
 
-    process4 = subprocess.run(['ls', '-a'],
-                              stdout=subprocess.PIPE,
-                              universal_newlines=True)
+    # process2 = subprocess.run(['ls'],
+    #                           stdout=subprocess.PIPE,
+    #                           universal_newlines=True)
+    # print("process2:")
+    # print(process2.stdout)
 
-    # print("process4: " + process4.stdout)
-    # print(type(process4.stdout))
-    print("testing" + process4.stdout + "length:" + str(len(process4.stdout)))
-    for char in process4.stdout:
-        print("char " + str(ord(char)) + " = " + char)
+    # # process3 = subprocess.run(['ls -a'],
+    # #                          stdout=subprocess.PIPE,
+    # #                          universal_newlines=True)
+    # # print("process3:")
+    # # print(process3.stdout)
 
-    test = process4.stdout.split()
-    test = ' '.join(test)
-    print("test: " + test)
+    # process4 = subprocess.run(['ls', '-a'],
+    #                           stdout=subprocess.PIPE,
+    #                           universal_newlines=True)
 
-    process4 = subprocess.run(['podman', 'images'],
+    # # print("process4: " + process4.stdout)
+    # # print(type(process4.stdout))
+    # print("testing" + process4.stdout + "length:" + str(len(process4.stdout)))
+    # for char in process4.stdout:
+    #     print("char " + str(ord(char)) + " = " + char)
+
+    # test = process4.stdout.split()
+    # test = ' '.join(test)
+    # print("test: " + test)
+
+    process4 = subprocess.run(['podman', 'images --format "{{.Repository}} {{.Tag}} {{.ID}} {{.Created}} {{.Size}}|"'],
                               stdout=subprocess.PIPE,
                               universal_newlines=True)
 
@@ -63,7 +63,17 @@ def get_stores():
     podman_images = ' '.join(podman_images)
     print("podman_images: " + podman_images)
 
-    return f"Main Page for Flask Backend, test message: {process.stdout} | ls: {process2.stdout} | ls -a: {process4.stdout} | podman images: {podman_images} > done"
+    # return f"Main Page for Flask Backend, test message: {process.stdout} | ls: {process2.stdout} | ls -a: {process4.stdout} | podman images: {podman_images} > done"
+
+    test_images_output = "REPOSITORY TAG IMAGE ID CREATED SIZE docker.io/library/nginx latest 6678c7c2e56c 4 weeks ago 131 MB docker.io/library/alpine latest e7d92cdc71fe 2 months ago 5.86 MB | podman images: REPOSITORY TAG IMAGE ID CREATED SIZE docker.io/library/nginx latest 6678c7c2e56c 4 weeks ago 131 MB docker.io/library/alpine latest e7d92cdc71fe 2 months ago 5.86 MB"
+    images_info = test_images_output
+    # images_info = test_images_output.split("SIZE ")[1]
+
+    # REPOSITORY    TAG      IMAGE ID    CREATED    SIZE
+    # podman images --format "{{.Repository}} {{.Tag}} {{.ID}} {{.Created}} {{.Size}}|"
+    # podman images --format "{{}}"
+
+    return f"Info: {images_info} END"
 
 
 # GET /store
