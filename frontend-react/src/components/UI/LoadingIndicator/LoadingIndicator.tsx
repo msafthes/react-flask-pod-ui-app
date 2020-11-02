01bb5ec9836171da4e560e27ee2aa306873abef2
Martin Safar <1martinsafar@gmail.com>
2020-11-02 12:19:26 +0100

    added LoadingIndicator

---
+++ frontend-react/src/components/UI/LoadingIndicator/LoadingIndicator.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import css from './LoadingIndicator.module.css';
+
+const LoadingIndicator = () => (
+    <div className={css.Loading_Indicator}>
+        <div />
+        <div />
+        <div />
+        <div />
+    </div>
+);
+
+export default LoadingIndicator;
