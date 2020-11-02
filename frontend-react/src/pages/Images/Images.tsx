8b807f203314a95b8545ff144b440026ce287fb0
Martin Safar <1martinsafar@gmail.com>
2020-11-02 12:21:42 +0100

    Images page refactored, added TypeScript

--- frontend-react/src/pages/Images/Images.tsx
+++ frontend-react/src/pages/Images/Images.tsx
@@ -1,35 +1,45 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import css from './Images.module.css';
-
-// import axios from 'axios';
-
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import * as actions from './../../store/actions/index';
+import { ThunkDispatch } from 'redux-thunk';
+import { AnyAction } from 'redux';
+import * as actions from '../../store/actions/index';
+import { AppState } from '../../store';
+
+import css from './Images.module.css';
+import LoadingIndicator from '../../components/UI/LoadingIndicator/LoadingIndicator';
+import { Image } from '../../models/Models';
 
 
-const Images = props => {
-    const { fetchImages, imagesData } = props;
+interface IImagesProps {
+    images: Image[],
+    loading: boolean,
+    fetchImages: Function
+}
+
+const Images = (props: IImagesProps) => {
+    const { fetchImages, images } = props;
 
     useEffect(() => {
         fetchImages();
     }, [fetchImages]);
 
-    const images = props.images.images;
-
-
-    // const [setImages] = useState([]);
-
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         const result = await axios(
-    //             'http://127.0.0.1:5000/images',
-    //         );
-    //         setImages(result.data);
-    //     };
-    //     fetchData();
-    // }, []);
 
     const imagesTitleClasses = [css.Content, css.Heading];
+    let content = <div className={css.Wrapper}><LoadingIndicator /></div>
+
+    if (!props.loading) {
+        content = <div>
+            {images && images.length && images.map((image, i) => {
+                return <div className={css.Content} key={image.id}>
+                    <div className={css.Repository}>{image.repository}</div>
+                    <div className={css.Tag}>{image.tag}</div>
+                    <div className={css.Id}>{image.id}</div>
+                    <div className={css.Created}>{image.created}</div>
+                    <div className={css.Size}>{image.size}</div>
+                </div>
+            })}
+        </div>
+    }
 
     return (
         <div className={css.Images}>
@@ -44,39 +54,23 @@ const Images = props => {
                         <div className={css.Created}>Created</div>
                         <div className={css.Size}>Size</div>
                     </div>
-                    {/* {props.imagesData.map((image, i) => {
-                        return <div className={css.Content} key={image.key}>
-                            <div className={css.Repository}>{image.repository}</div>
-                            <div className={css.Tag}>{image.tag}</div>
-                            <div className={css.Id}>{image.id}</div>
-                            <div className={css.Created}>{image.created}</div>
-                            <div className={css.Size}>{image.size}</div>
-                        </div>
-                    })} */}
-                    {images && images.length && images.map((image, i) => {
-                        return <div className={css.Content} key={image.key}>
-                            <div className={css.Repository}>{image.repository}</div>
-                            <div className={css.Tag}>{image.tag}</div>
-                            <div className={css.Id}>{image.id}</div>
-                            <div className={css.Created}>{image.created}</div>
-                            <div className={css.Size}>{image.size}</div>
-                        </div>
-                    })}
+                    {content}
                 </div>
             </div>
         </div>
     );
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state: AppState) => {
     return {
-        images: state.images
+        images: state.images.images,
     };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch: ThunkDispatch<any, any, AnyAction>) => {
     return {
-        fetchImages: () => dispatch(actions.fetchImages())
+        fetchImages: () =>
+            dispatch(actions.fetchImages()),
     };
 };
 
