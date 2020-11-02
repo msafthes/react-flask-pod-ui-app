34c81276e64e98c52476ae5ac92041b38c2c8045
Martin Safar <1martinsafar@gmail.com>
2020-11-02 12:19:08 +0100

    Added Model (TypeScript interface) for Images

---
+++ frontend-react/src/models/Models.tsx
@@ -0,0 +1,8 @@
+export interface Image {
+    key: number,            // 0
+    created: string,        // "6 weeks ago"
+    id: string,             // "6678c7c2e56c"
+    repository: string,     // "docker.io/library/nginx"
+    size: string,           // "131 MB"
+    tag: string             // "1.17.10-alpine-perl"
+}
