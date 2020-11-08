68b9b2bd8cc0ac3a8f0c3e756997c0ab1f51e2e1
Martin Safar <1martinsafar@gmail.com>
2020-11-08 11:49:42 +0100

    Backend: CONTAINER ID  IMAGE                                        COMMAND               CREATED         STATUS             PORTS                   NAMES 54a48d41f6d9  registry.fedoraproject.org/f29/httpd:latest  /usr/bin/run-http...  52 minutes ago  Up 52 minutes ago  0.0.0.0:8080->8080/tcp  laughing_bassi /containers API created

--- backend-flask/main.py
+++ backend-flask/main.py
@@ -7,10 +7,9 @@ import subprocess
 app = Flask("__main__")
 CORS(app)
 
-
 # GET /images
 @app.route('/images', methods=['GET', 'POST'])
-def get_stores():
+def get_images():
 
     # Example: separating each info with #
     # docker.io/library/nginx#latest#6678c7c2e56c#4 weeks ago #131 MB
@@ -40,6 +39,39 @@ def get_stores():
         images["images"].append(image)
 
     return jsonify(images)
+
+# GET /containers
+@app.route('/containers', methods=['GET', 'POST'])
+def get_containers():
+
+    # Example: separating each info with #
+    # 54a48d41f6d9#registry.fedoraproject.org/f29/httpd:latest#/usr/bin/run-http...#2 minutes ago#0.0.0.0:8080->8080/tcp#laughing_bassi
+
+    process4 = subprocess.run(['podman', 'ps', '--format', '{{.ID}}#{{.Image}}#{{.Command}}#{{.RunningFor}}#{{.Ports}}#{{.Names}}'],
+                              stdout=subprocess.PIPE,
+                              universal_newlines=True)
+
+    podman_containers_array = process4.stdout.split('\n')
+    podman_containers_array.pop()  # Removing the last '' empty part after split
+
+    containers = {"containers": []}
+
+    for item in podman_containers_array:
+
+        container_parts = item.split("#")
+
+        container = {
+            'id': container_parts[0],
+            'image': container_parts[1],
+            'command': container_parts[2],
+            'created': container_parts[3],
+            'ports': container_parts[4],
+            'names': container_parts[5]
+        }
+
+        containers["containers"].append(container)
+
+    return jsonify(containers)
 
 # GET /
 @app.route('/', methods=['GET', 'POST'])
