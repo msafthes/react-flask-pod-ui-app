cba674fc67a5ffff3bfb08fe399d7f04cd02fea1
Martin Thesis <1martinsafar@gmail.com>
2020-11-28 19:08:19 +0100

    Frontend - added Material UI, Checkboxes, simple select feature

--- frontend-react/src/pages/Images/Images.tsx
+++ frontend-react/src/pages/Images/Images.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk';
 import { AnyAction } from 'redux';
@@ -8,6 +8,9 @@ import { AppState } from '../../store';
 import css from './Images.module.css';
 import LoadingIndicator from '../../components/UI/LoadingIndicator/LoadingIndicator';
 import { Image } from '../../models/Models';
+
+import Grid from '@material-ui/core/Grid';
+import Checkbox from '@material-ui/core/Checkbox';
 
 
 interface IImagesProps {
@@ -21,27 +24,49 @@ interface IImagesProps {
 const Images = (props: IImagesProps) => {
     const { fetchImages, images } = props;
 
+    const [localImages, setLocalImages] = useState<any>({});
+
     useEffect(() => {
         fetchImages();
     }, [fetchImages]);
 
-    // const images = props.imagesDataTest
+    useEffect(() => {
+        for (const [key, value] of Object.entries(images)) {
+            localImages[value.id] = false
+        }
+    }, []);
+
+    console.log(localImages);
+
+    const handleCheckboxChange = changeEvent => {
+        const { id } = changeEvent.target;
+        const old = { ...localImages };
+
+        for (const [key, value] of Object.entries(old)) {
+            if (id === key) {
+                old[key] = !old[key];
+                setLocalImages(old);
+            }
+        }
+    };
 
     const imagesTitleClasses = [css.Content, css.Heading];
     let content = <div className={css.Wrapper}><LoadingIndicator /></div>
 
     if (!props.loading) {
-        content = <div>
+        content = <Grid container direction="column">
             {images && images.length && images.map((image, i) => {
-                return <div className={css.Content} key={image.id}>
-                    <div className={css.Repository}>{image.repository}</div>
-                    <div className={css.Tag}>{image.tag}</div>
-                    <div className={css.Id}>{image.id}</div>
-                    <div className={css.Created}>{image.created}</div>
-                    <div className={css.Size}>{image.size}</div>
-                </div>
+                return <Grid item container className={css.Content} key={image.id}>
+                    <Checkbox color="primary" onClick={handleCheckboxChange} id={image.id} />
+                    <Grid className={css.Repository}>{image.repository}</Grid>
+                    <Grid className={css.Tag}>{image.tag}</Grid>
+                    <Grid className={css.Id}>{image.id}</Grid>
+                    <Grid className={css.Created}>{image.created}</Grid>
+                    <Grid className={css.Size}>{image.size}</Grid>
+                </Grid>
+
             })}
-        </div>
+        </Grid>
     }
 
     return (
@@ -51,6 +76,7 @@ const Images = (props: IImagesProps) => {
                 <p>Showing information about images based on the `podman images` command</p>
                 <div className={css.Info}>
                     <div className={imagesTitleClasses.join(' ')}>
+                        <Checkbox color="primary" />
                         <div className={css.Repository}>Repository</div>
                         <div className={css.Tag}>Tag</div>
                         <div className={css.Id}>ID</div>
