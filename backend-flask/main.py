40369688067ef8add5fa52f4c08ebde34e037cf8
Martin Thesis <1martinsafar@gmail.com>
2020-12-02 11:10:18 +0100

    Backend: added remove images, remove containers, created helper functions, check if not empty

--- backend-flask/main.py
+++ backend-flask/main.py
@@ -7,10 +7,11 @@ import subprocess
 app = Flask("__main__")
 CORS(app)
 
-# GET /images
-@app.route('/images', methods=['GET', 'POST'])
-def get_images():
+############################################################################################################################################################
+# Functions
+############################################################################################################################################################
 
+def podman_images():
     # Example: separating each info with #
     # docker.io/library/nginx#latest#6678c7c2e56c#4 weeks ago #131 MB
     # docker.io/library/alpine#latest#e7d92cdc71fe#2 months ago#5.86 MB
@@ -38,12 +39,9 @@ def get_images():
 
         images["images"].append(image)
 
-    return jsonify(images)
+    return images
 
-# GET /containers
-@app.route('/containers', methods=['GET', 'POST'])
-def get_containers():
-
+def podman_ps():
     # Example: separating each info with #
     # 54a48d41f6d9#registry.fedoraproject.org/f29/httpd:latest#/usr/bin/run-http...#2 minutes ago#0.0.0.0:8080->8080/tcp#laughing_bassi
 
@@ -70,6 +68,91 @@ def get_containers():
         }
 
         containers["containers"].append(container)
+    
+    return containers
+
+############################################################################################################################################################
+# REST API
+############################################################################################################################################################
+
+# GET /images
+@app.route('/images', methods=['GET'])
+def get_images():
+
+    images = podman_images()
+
+    return jsonify(images)
+
+# POST /images
+@app.route('/images', methods=['DELETE'])
+def remove_images():
+    image_ids = request.get_json().get("IDs")
+    print("image_ids:")
+    print(image_ids)
+
+    count = len(image_ids)
+
+    all_ids = " ".join(image_ids)
+
+    # for id in image_ids:
+    #     print("Adding ID for removal:", id)
+    #     all_ids = " ".join(all_ids)
+    #     # subprocess.run(['podman', 'rmi', ''],
+    #     #                       stdout=subprocess.PIPE,
+    #     #                       universal_newlines=True)
+
+    print("count:", count)
+    print("all_ids:")
+    print(all_ids)
+    print(len(all_ids))
+
+    # subprocess.run(['podman', 'rmi', all_ids],
+    #                         stdout=subprocess.PIPE,
+    #                         universal_newlines=True)
+
+    # subprocess.run("podman rmi {image_ids}".format(image_ids=all_ids), shell=True, capture_output=True).stdout
+    command = "podman rmi {0}".format(all_ids)
+    print("command:")
+    print(command)
+
+    if count != 0:
+        subprocess.run("{0}".format(command), shell=True, capture_output=True).stdout
+    
+    images = podman_images()
+
+    return jsonify(images)
+
+# GET /containers
+@app.route('/containers', methods=['GET'])
+def get_containers():
+
+    containers = podman_ps()
+
+    return jsonify(containers)
+
+# DELETE /containers
+@app.route('/containers', methods=['DELETE'])
+def remove_containers():
+    container_ids = request.get_json().get("IDs")
+    print("container_ids:")
+    print(container_ids)
+
+    count = len(container_ids)
+
+    all_ids = " ".join(container_ids)
+
+    print("count:", count)
+    print("all_ids:")
+    print(all_ids)
+
+    command = "podman rm {0}".format(all_ids)
+    print("command:")
+    print(command)
+
+    if count != 0:
+        subprocess.run("{0}".format(command), shell=True, capture_output=True).stdout
+    
+    containers = podman_ps()
 
     return jsonify(containers)
 
