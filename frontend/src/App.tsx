696839e645326a3af95154f5ecb2c7d74da88b80
Martin Thesis <1martinsafar@gmail.com>
2021-05-18 19:23:34 +0200

    removed unused code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import throttle from 'lodash.throttle';
 
 import './App.css';
-import { Connection } from './models/Models';
 import { AppState } from './store';
 import { imagesDataTest, containersDataTest, volumesDataTest } from './testData/testData';
 
